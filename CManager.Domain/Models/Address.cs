770bf0b01931837a63dd352bb848f78fa15638b0
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2025-12-29 16:41:14 +0100

    Added data to Customer and Address classes

--- CManager.Domain/Models/Address.cs
+++ CManager.Domain/Models/Address.cs
@@ -4,5 +4,7 @@ namespace CManager.Domain.Models;
 
 public class Address
 {
-
+    public string Street {get; set;} = null!;
+    public string PostalCode {get; set;} = null!;
+    public string City {get; set;} = null!;
 }
