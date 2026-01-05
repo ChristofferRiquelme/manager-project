c090e1a7720ff36e83a9596ce9d6b958a5b576a2
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2026-01-05 16:31:27 +0100

    Added a map for Services

--- CManager.Application/obj/Debug/net10.0/CManager.Application.AssemblyInfo.cs
+++ CManager.Application/obj/Debug/net10.0/CManager.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CManager.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e11c19973465ff2e20e19d71a7a10d8f5ba4f3de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e09e21dd5838c092a97533305dcc2397f1a18c49")]
 [assembly: System.Reflection.AssemblyProductAttribute("CManager.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CManager.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
