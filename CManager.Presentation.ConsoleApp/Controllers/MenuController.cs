ad36d6867e3b0f836409a7962237e606570ca696
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2026-01-06 17:50:33 +0100

    Added some code to MenuController

--- CManager.Presentation.ConsoleApp/Controllers/MenuController.cs
+++ CManager.Presentation.ConsoleApp/Controllers/MenuController.cs
@@ -1,8 +1,58 @@
 using System;
+using CManager.Application.Interfaces;
 
 namespace CManager.Presentation.ConsoleApp.Controllers;
 
 public class MenuController
 {
+    private ICustomerService _customerService;
 
+    public MenuController(ICustomerService customerService)
+    {
+        _customerService = customerService;
+    }
+
+    public void Run()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Skapa kund");
+            Console.WriteLine("2. Visa kunder");
+            Console.WriteLine("3. Visa specifik kund");
+            Console.WriteLine("4. Ta bort kund");
+            Console.WriteLine("0. Avsluta");
+
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1": CreateCustomer(); break;
+                case "2": ShowCustomers(); break;
+                case "3": ShowCustomer(); break;
+                case "4": DeleteCustomer(); break;
+                case "0": return;
+            }
+
+            Console.WriteLine("\nTryck på valfri tangent för att fortsätta...");
+            Console.ReadKey();
+        }
+    }
+
+    private void CreateCustomer()
+    {
+        
+    }
+    private void ShowCustomers()
+    {
+        
+    }
+    private void ShowCustomer()
+    {
+        
+    }
+    private void DeleteCustomer()
+    {
+        
+    }
 }
--- CManager.Presentation.ConsoleApp/Program.cs
+++ CManager.Presentation.ConsoleApp/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using CManager.Application.Services;
+using CManager.Infrastructure.Repositories;
+using CManager.Presentation.ConsoleApp.Controllers;
+
+var repository = new CustomerRepository();
+var service = new CustomerService(repository);
+var controller = new MenuController(service);
+
+controller.Run();
